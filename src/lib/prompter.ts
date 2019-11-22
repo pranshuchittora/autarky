5fd6041a45470d95f6afdb9b6d377f393f816d49
Pranshu Chittora <pranshuchittora17@gmail.com>
2019-11-23 02:34:34 +0530

    feat: migrated to inquirer
    - Migrated from prompts to inquirer

--- src/lib/prompter.ts
+++ src/lib/prompter.ts
@@ -1,56 +1,76 @@
-import prompts from "prompts";
 import chalk from "chalk";
+import * as inquirer from "inquirer";
 
-import { CHANGE_AGE_CAP, UPDATE_DIRS_LIST } from "../redux/actionTypes";
 import store from "../redux/index";
-import { TimeRelative } from "./time";
+import { CHANGE_AGE_CAP, UPDATE_DIRS_LIST } from "../redux/actionTypes";
 import { promptListParser } from "./utils";
 import { removeDirBulk } from "./removeDir";
 
-export async function promptMultiSelectDir(ListDir: Object[]) {
-  const parsedChoices = await promptListParser(ListDir);
+/**
+ * Asks for the module age
+ */
+export async function promptAgeSelect() {
+  const response = await inquirer.prompt([
+    {
+      type: "number",
+      name: "age",
+      message: "How old node_modules you wanna delete? (months)"
+    }
+  ]);
 
-  const response = await prompts({
-    type: "multiselect",
-    name: "value",
-    message: "Select Directories",
-    choices: parsedChoices,
-    hint: "- Space to select. Return to submit"
-  });
+  store.dispatch({ type: CHANGE_AGE_CAP, payload: { file_age: response.age } });
+}
+
+/**
+ * Multiselect for the diles to be deleted
+ * @param ListDir
+ */
+export async function promptMultiSelectDir(ListDir: Object[]) {
+  const parsedChoices = promptListParser(ListDir);
+
+  const response = await inquirer.prompt([
+    {
+      type: "checkbox",
+      message: "Select directories to be deleted",
+      name: "selectedDirs",
+      pageSize: 20,
+      choices: parsedChoices,
+      validate: function(answer) {
+        if (answer.length < 1) {
+          return "You must choose at least one topping.";
+        }
+
+        return true;
+      }
+    }
+  ]);
 
   store.dispatch({
     type: UPDATE_DIRS_LIST,
     payload: {
-      dir_list: response.value
+      dir_list: response.selectedDirs
     }
   });
-}
-
-export async function promptAgeSelect() {
-  const response = await prompts({
-    type: "number",
-    name: "age",
-    // onCancel: cancelPrompt,
-
-    message: "How old node_modules you wanna delete? (months)"
-  });
-  store.dispatch({ type: CHANGE_AGE_CAP, payload: { file_age: response.age } });
 }
 
 export async function promptDeleteConfirm() {
   const dirList = store.getState().config.dir_list;
   const count = Array.isArray(dirList) && dirList.length;
-  const response = await prompts({
-    type: "confirm",
-    name: "confirmation",
-    // onCancel: cancelPrompt,
-    message: `Confirm deleteing ${count} directories ?`
-  });
-  // store.dispatch({ type: CHANGE_AGE_CAP, payload: { file_age: response.age } });
+
+  const response = await inquirer.prompt([
+    {
+      type: "confirm",
+      message: `Confirm deleteing ${count} directories ?`,
+      name: "confirmation"
+    }
+  ]);
+
   if (response.confirmation === true) {
     removeDirBulk(dirList);
     process.stdout.write(
-      chalk.green(`\nDeleted directories successfully 🎉\n\n`)
+      chalk.green(`\nDeleted ${count} directories successfully 🎉\n\n`)
     );
+  } else {
+    process.stdout.write(chalk.red(`\nBetter luck next time. 😔\n\n`));
   }
 }
