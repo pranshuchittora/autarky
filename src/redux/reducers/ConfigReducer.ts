188ba4e98a56bbc2b50978a184474cb1eee81eb2
Pranshu Chittora <pranshuchittora17@gmail.com>
2019-11-23 19:21:52 +0530

    test: added test for the redux
    - Tests for Reducers
    - Tests for action types
    
    fix: typo fix
    
    fix: removed the interface for the redux store object

--- src/redux/reducers/ConfigReducer.ts
+++ src/redux/reducers/ConfigReducer.ts
@@ -1,17 +1,21 @@
 import { CHANGE_AGE_CAP, UPDATE_DIRS_LIST } from "../actionTypes";
 
 const InitialState = { file_age: 0, dir_list: [] };
-export function R_Config(
+
+export const R_Config = (
   state = InitialState,
   action: { payload: Object | any; type: String }
-) {
+) => {
   let newState = { ...state };
   const payload = action.payload;
   switch (action.type) {
     case CHANGE_AGE_CAP:
-      newState.file_age = payload.file_age;
+      if (isNaN(payload.file_age) !== true) {
+        newState.file_age = payload.file_age;
+      }
     case UPDATE_DIRS_LIST:
+      // console.log(FilterString(payload.dir_list));
       newState.dir_list = payload.dir_list;
   }
   return newState;
-}
+};
